4b6ee743076b3e87a374700bc5421035b8aa2a88
lllGERAlll <gerardoligorio17@gmail.com>
2025-05-16 17:50:48 -0600

    Ejercicio a

---
+++ Practica1/Ejercicio_a.js
@@ -0,0 +1,10 @@
+let nombre = "Arnamdo"; 
+const edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`;
+
+console.log(saludo);
+
+//Muestra en consola el contenido de saludo
