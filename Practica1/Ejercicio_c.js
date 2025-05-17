d434d313b677b71e32c9ff7019dbe6231f2fdf90
lllGERAlll <gerardoligorio17@gmail.com>
2025-05-16 18:12:44 -0600

    Ejercicio c

---
+++ Practica1/Ejercicio_c.js
@@ -0,0 +1,6 @@
+const nombre = "Gerardo"
+const edad = 20
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`
+
+console.log(saludoPersonalizado(nombre, edad))
